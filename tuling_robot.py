7bc29faa15481d37e906ae05565f52cf7b9b74f8
wupengchoy <15962780713@163.com>
2019-05-16 15:19:11 +0800

    update robot

--- tuling_robot.py
+++ tuling_robot.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         此apikey为wxpy自带apikey，有使用次数限制，建议自己免费申请一个
         图灵机器人免费申请地址 http://www.tuling123.com
     """
-    apikey = '7c8cdb56b0dc4450a8deef30a496bd4c'
+    apikey = '6709b4b71a7949b1b2b57c06d875b56b'
     api_url = 'http://www.tuling123.com/openapi/api'
     data = {'key': apikey, 'info': '你好'}
     req = requests.post(api_url, data=data).text
