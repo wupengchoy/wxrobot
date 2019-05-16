429672a5ad8fe07e397e399a5d054249c36f2494
wupengchoy <15962780713@163.com>
2019-05-16 14:57:16 +0800

    update

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ bot_master_name = ''  # 使用备注名更安全，只允许一个，可远程�
 
 # 监听某些好友群聊，如老板
 is_listen_friend = False
-listen_friend_names = '猪哥'  # 需要监听的人名称，使用备注名更安全，允许多个用|分隔，如：主管|项目经理|产品狗
-listen_friend_groups = 'Python新手交流'  # 在这些群里监听好友说的话，匹配模式：包含“唯一集团工作群”的群
+listen_friend_names = '李玉强'  # 需要监听的人名称，使用备注名更安全，允许多个用|分隔，如：主管|项目经理|产品狗
+listen_friend_groups = '纵使青春留不住'  # 在这些群里监听好友说的话，匹配模式：包含“唯一集团工作群”的群
 
 
 # 转发信息至群
@@ -26,5 +26,3 @@ forward_groups = 'Python新手交流'  # 需要将消息转发的群，匹配模
 # 群分享监控
 is_listen_sharing = False
 listen_sharing_groups = 'Python新手交流'  # 监控群分享，匹配模式同上
-
-
--- wx_reply.py
+++ wx_reply.py
@@ -23,6 +23,8 @@ def keyword_reply(msg):
     """关键字回复"""
     if '你叫啥' in msg.text or '你叫啥名字' in msg.text:
         return msg.reply('沃德天·维森莫·拉莫帅·帅德布耀')
+    if '吴老二' in msg.text:
+        return msg.reply('是个大帅哥')
     pass
 
 
